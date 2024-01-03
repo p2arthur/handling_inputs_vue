ecd7003b0bcf2d12f0922c955d82f106b46c1156
Arthur Rabelo <arthurrabelopro@gmail.com>
2024-01-02 23:51:34 -0300

    (feat) finished the project by adding a v-model to get input values and set to the newMember global state - when clicking submit it triggers the add member method that pushes the new member to the members array that gets rerendered

--- scripts.js
+++ scripts.js
@@ -13,11 +13,25 @@ members = [
 
 const handlingForms = {
   data() {
-    return { members: window.members };
+    return {
+      members: window.members,
+      newMember: {},
+    };
   },
 
   methods: {
-    addMember: () => {},
+    addMember: function () {
+      if (
+        !this.newMember.fname ||
+        !this.newMember.lname ||
+        !this.newMember.instrument
+      )
+        window.alert('Fill all the fields before adding a new member');
+      return;
+      this.members.push(this.newMember);
+      this.newMember = {};
+      console.log(this.members);
+    },
   },
 };
 
